9aea6f261f0f2fef0fb2b78d8a6327b2daa1185b
tafhimkhan39 <47645217+tafhimkhan39@users.noreply.github.com>
2023-05-16 14:57:45 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -91,3 +91,72 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.route('/', methods=["GET", "POST"])
+def computers():
+    computers = Computers.query.all()
+    return render_template('computers.html', computers = computers)
+
+@app.route('/<int:computer_id>', methods=["GET"])
+def getComputer(computer_id):
+    computer = Computers.query.get(computer_id)
+    return render_template('computer.html', computer=computer)
+
+@login_required
+@app.route("/cart")
+def cart():
+    user_id = current_user.id
+    cart_items = Cart.query.filter_by(user_id=user_id).all()
+    computers = []
+    final_total = 0
+    for item in cart_items:
+        computer = Computers.query.get(item.computer_id)
+        computer.quantity = item.quantity
+        final_total += computer.price * computer.quantity
+        computers.append(computer)
+    return render_template('cart.html', cart=computers, final_total = final_total)
+
+
+@app.route('/<int:computer_id>/add_to_cart', methods=["POST", "GET"])
+def add_to_cart(computer_id):
+
+    if current_user.is_authenticated:
+        user_id = current_user.id
+        cart_item = Cart.query.filter_by(computer_id=computer_id, user_id=user_id).first()
+        if cart_item:
+            cart_item.quantity += 1
+            cart_item.saveToDB()
+        else:
+            cart = Cart(computer_id=computer_id, user_id=user_id, quantity=1)
+            cart.saveToDB()
+    else:
+        flash('You need to log in to add items to your cart', category='danger')
+        return redirect(url_for('auth.loginPage'))
+    return redirect(url_for('cart'))
+
+@app.route('/cart/<int:computer_id>/remove', methods=["POST", "GET"])
+def remove_from_cart(computer_id):
+    user_id = current_user.id
+    cart_item = Cart.query.filter_by(computer_id=computer_id, user_id=user_id).first()
+
+    if not cart_item:
+        return redirect(url_for('cart'))
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.saveToDB()
+    else:
+        cart_item.deleteFromDB()
+
+    return redirect(url_for('cart'))
+
+@app.route('/cart/clear', methods=["POST", "GET"])
+def clear_cart():
+    user_id = current_user.id
+    cart_items = Cart.query.filter_by(user_id=user_id).all()
+
+    for cart_item in cart_items:
+        cart_item.deleteFromDB()
+
+    return redirect(url_for('cart'))
+
